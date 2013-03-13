ddf599667a1120a9b36884df21cac2469709e695
Francisco Fernández Castaño <fran@desktop.(none)>
2013-03-13 21:05:29 +0100

    Rails 4 Skelenton for toListen app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tolisten::Application.config.secret_key_base = '514d2fab5714916a73f30a33a23196d3fd9869087d527f8c534e75ea527fc24343b555d26920ea9c71f6ef737c8fdf62b7971fe1b9e66e126cc4b5b4be1358d3'
